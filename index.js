ad33b48ae459abb068f75b8cce539bb5a694073a
nur922184 <nuruzzaman.engbd@gmail.com>
2025-01-15 01:25:45 +0600

    biodata get requset empliment

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hq6na.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // const uri = `mongodb+srv://assignment12:tktwS882WEO1VZBQ@cluster0.hq6na.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,6 +26,37 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const BioDataCollection = client.db('LifeLinkDB').collection('biodata');
+    const userCollection = client.db('LifeLinkDB').collection('users');
+
+    //biodata related 
+    app.get('/biodata', async (req, res) => {
+      const result = await BioDataCollection.find().toArray();
+      res.send(result)
+  })
+  app.get('/biodata/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await BioDataCollection.findOne(query);
+    res.send(result);
+})
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ massage: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -37,10 +68,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) =>{
-res.send('New final project running now.')
+app.get('/', (req, res) => {
+  res.send('New final project running now.')
 })
 
-app.listen(port, ()=>{
-    console.log(`New final project running on port:${port}`)
+app.listen(port, () => {
+  console.log(`New final project running on port:${port}`)
 })
