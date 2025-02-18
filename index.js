bbdd54860b9878097d7f296841cb06739b140db4
nur922184 <nuruzzaman.engbd@gmail.com>
2025-02-18 14:53:04 +0600

    update some

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
     const BioDataCollection = client.db('LifeLinkDB').collection('biodata');
@@ -34,11 +34,6 @@ async function run() {
     const userCollection = client.db('LifeLinkDB').collection('users');
     const paymentCollection = client.db('LifeLinkDB').collection('payments');
     const successStoriesDataCollection = client.db('LifeLinkDB').collection('successStoriesData');
-
-
-
-    
-
 
 
     // medile ware-
@@ -378,10 +373,8 @@ async function run() {
     });
 
 
-
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
